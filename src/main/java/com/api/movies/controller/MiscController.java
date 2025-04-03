00d896ae29f6b77fe74b81446e792c521792b56a
Adrian Herrmann <lumbia.85@gmail.com>
2025-04-03 14:06:59 -0300

    Adicionado formulário e métodos no controller

--- src/main/java/com/api/movies/controller/MiscController.java
+++ src/main/java/com/api/movies/controller/MiscController.java
@@ -1,14 +1,25 @@
 package com.api.movies.controller;
 
+import com.api.movies.model.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class MiscController {
     
     @GetMapping("/cadastrar")
-    public String mostrarCadastro() {
+    public String mostraCadastro(Model model) {
+        model.addAttribute("filme", new Filme());
         return "cadastrar";
+    }
+    
+    @PostMapping("/cadastrar")
+    public String recebeCadastro(Model model, @ModelAttribute Filme filme) {
+        model.addAttribute("filme", filme);
+        return "filme";
     }
 }
